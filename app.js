718020f21a2d01d577ea2516b3f2ca90e87e06b7
dk-jaiswal-77 <dkjaiswal771999@gmail.com>
2022-06-06 09:11:58 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// express app 
+const app = express();
+
+
+
+
+
+
+
+
+// port
+const port = 3000;
+// listening to port
+app.listen(port, console.log(`server listening to port ${port}...`));
